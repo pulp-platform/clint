301ddbf2702cdd328da9e4e2cc3d9fb5228c5c20
Tim Fischer <fischeti@iis.ee.ethz.ch>
2025-11-30 13:39:31 +0100

    src: Update sources
    local `peakrdl-regblock` version was older

--- src/clint_reg.sv
+++ src/clint_reg.sv
@@ -102,12 +102,17 @@ module clint_reg (
         } mtime;
     } decoded_reg_strb_t;
     decoded_reg_strb_t decoded_reg_strb;
+    logic decoded_err;
     logic decoded_req;
     logic decoded_req_is_wr;
     logic [31:0] decoded_wr_data;
     logic [31:0] decoded_wr_biten;
 
     always_comb begin
+        automatic logic is_valid_addr;
+        automatic logic is_invalid_rw;
+        is_valid_addr = '1; // No error checking on valid address access
+        is_invalid_rw = '0;
         for(int i0=0; i0<2; i0++) begin
             decoded_reg_strb.msip[i0] = cpuif_req_masked & (cpuif_addr == 16'h0 + (16)'(i0) * 16'h4);
         end
@@ -117,6 +122,7 @@ module clint_reg (
         end
         decoded_reg_strb.mtime.low = cpuif_req_masked & (cpuif_addr == 16'hbff8);
         decoded_reg_strb.mtime.high = cpuif_req_masked & (cpuif_addr == 16'hbffc);
+        decoded_err = (~is_valid_addr | is_invalid_rw) & decoded_req;
     end
 
     // Pass down signals to next stage
