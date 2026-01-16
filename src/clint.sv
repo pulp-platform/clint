67450ba4e3f7767ab5072b559721e94c04b7e9d8
Tim Fischer <fischeti@iis.ee.ethz.ch>
2026-01-16 10:48:38 +0100

    clint: Fix size mismatch warning

--- src/clint.sv
+++ src/clint.sv
@@ -136,7 +136,7 @@ module clint_sync #(
 );
 
   logic [STAGES-1:0] reg_q;
-  `FF(reg_q, {reg_q[STAGES-2:0], serial_i}, 'h0)
+  `FF(reg_q, {reg_q[STAGES-2:0], serial_i}, '0)
   assign serial_o = reg_q[STAGES-1];
 
 endmodule
